397b5dc83a1e325a73b00cd0fe2c95dab7633c30
Aurooba Ahmed <hey@aurooba.com>
2022-01-31 16:18:33 -0700

    initial commit

---
+++ add-new-post.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Add New Post
+ * Plugin URI:      https://createwithrani.com
+ * Description:     Adds an "Add New" button to the Block Editor toolbar, so you can easily create new posts/pages/custom post types.
+ * Author:          Aurooba Ahmed
+ * Author URI:      https://createwithrani.com
+ * Text Domain:     add-new-post
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         add-new-post
+ */
+
+// Your code starts here.
