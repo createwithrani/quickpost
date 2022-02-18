2a6226280da0254226b923a23afe46274b0a7f51
Aurooba Ahmed <hey@aurooba.com>
2022-02-17 17:26:46 -0700

    update author uri to me

--- quickpost.php
+++ quickpost.php
@@ -4,7 +4,7 @@
  * Plugin URI:      https://createwithrani.com
  * Description:     Adds an "Add New" button to the Block Editor toolbar, so you can easily create new posts/pages/custom post types.
  * Author:          Aurooba Ahmed
- * Author URI:      https://createwithrani.com
+ * Author URI:      https://aurooba.com
  * Text Domain:     createwithrani-quickpost
  * Domain Path:     /languages
  * Version:         0.1.0
