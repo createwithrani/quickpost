6680d18648775ea4e9163fe378a90212bd13c4f1
Aurooba Ahmed <hey@aurooba.com>
2023-02-26 16:09:58 -0700

    add Tooltip for keyboard shortcut hinting for Add New button

--- src/add-button.js
+++ src/add-button.js
@@ -2,7 +2,7 @@
  * WordPress dependencies.
  */
 import { __ } from "@wordpress/i18n";
-import { ToolbarButton } from "@wordpress/components";
+import { Tooltip } from "@wordpress/components";
 import { addQueryArgs } from "@wordpress/url";
 
 /**
@@ -12,35 +12,54 @@ import { addQueryArgs } from "@wordpress/url";
  * @return {string} Return the rendered Quick Post Button
  */
 function AddNewPostButton({ postType, newPost, addNewLabel, singleLabel }) {
+	const platform =
+		navigator?.userAgentData?.platform || navigator?.platform || "unknown";
+	const isMac = platform.indexOf("Mac") > -1;
+	const isWindows = platform.indexOf("Win") > -1;
+	const isLinux = platform.indexOf("Linux") > -1;
+
+	function getAriaLabel() {
+		if (isMac) {
+			return __("Ctrl + Option + N", "createwithrani-quickpost");
+		} else if (isWindows || isLinux) {
+			return __("Alt + Shift + N", "createwithrani-quickpost");
+		} else if (isLinux) {
+			return __("Alt + Shift + N", "createwithrani-quickpost");
+		}
+	}
+
 	return (
-		<a
-			className="components-button components-toolbar__control is-secondary"
-			id="createwithrani-quick-post-button"
-			style={{
-				textTransform: "capitalize",
-				margin: "0 0 0 1em",
-				display: "block",
-				maxHeight: "36px",
-				minHeight: "36px",
-				display: "flex",
-				borderTopRightRadius: "0px",
-				borderBottomRightRadius: "0px",
-			}}
-			role="link"
-			disabled={!newPost}
-			aria-disabled={!newPost}
-			href={addQueryArgs("post-new.php", {
-				post_type: postType,
-			})}
-		>
-			<span>
-				{sprintf(
-					/* translators: %1$s: Name of current post type. */
-					__("Add new %1$s", "createwithrani-quickpost"),
-					singleLabel
-				)}
-			</span>
-		</a>
+		<Tooltip text={getAriaLabel()}>
+			<a
+				className="components-button components-toolbar__control is-secondary"
+				id="createwithrani-quick-post-button"
+				aria-label={getAriaLabel()}
+				style={{
+					textTransform: "capitalize",
+					margin: "0 0 0 1em",
+					display: "block",
+					maxHeight: "36px",
+					minHeight: "36px",
+					display: "flex",
+					borderTopRightRadius: "0px",
+					borderBottomRightRadius: "0px",
+				}}
+				role="link"
+				disabled={!newPost}
+				aria-disabled={!newPost}
+				href={addQueryArgs("post-new.php", {
+					post_type: postType,
+				})}
+			>
+				<span>
+					{sprintf(
+						/* translators: %1$s: Name of current post type. */
+						__("Add new %1$s", "createwithrani-quickpost"),
+						singleLabel
+					)}
+				</span>
+			</a>
+		</Tooltip>
 	);
 }
 
