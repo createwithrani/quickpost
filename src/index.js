ff839a8f300c06dc3fdfff7764e2a499f335c3a9
Aurooba Ahmed <hey@aurooba.com>
2023-05-02 09:26:54 -0600

    add view-post-button to the DOM

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ import domReady from "@wordpress/dom-ready";
  * Internal dependencies.
  */
 import QuickPostButton from "./quick-post";
-import { listenForKeyboardShortcut } from "./utils";
+import ViewPostButton from "./view-post-button";
+import { listenForKeyboardShortcut, insertAfter } from "./utils";
 
 /**
  * Let's subscribe (because I finally understand what this does better)
@@ -30,6 +31,13 @@ subscribe(() => {
 			".edit-post-header__toolbar"
 		);
 
+		const editorToolbarSettings = document.querySelector(
+			".edit-post-header__settings"
+		);
+		const viewButtonWrapper = document.createElement("div");
+		viewButtonWrapper.id = "createwithrani-view-post-button-wrapper";
+		viewButtonWrapper.style.cssText = "display:flex;";
+
 		// If toolbar doesn't exist, we can't continue
 		if (!editorToolbar) {
 			return;
@@ -41,14 +49,20 @@ subscribe(() => {
 		buttonWrapper.id = "createwithrani-quick-post-button-wrapper";
 		buttonWrapper.style.cssText = "display:flex;";
 
-		// add empty div to the toolbar so we can fill it.
+		// add empty divs to the toolbars so we can fill it.
 		editorToolbar.appendChild(buttonWrapper);
+		insertAfter(editorToolbarSettings.children[1], viewButtonWrapper);
 
 		render(
 			<QuickPostButton visibility={true} />,
 			document.getElementById("createwithrani-quick-post-button-wrapper")
 		);
+
+		render(
+			<ViewPostButton />,
+			document.getElementById("createwithrani-view-post-button-wrapper")
+		);
 	});
 });
 
-document.addEventListener('keydown', listenForKeyboardShortcut);
+document.addEventListener("keydown", listenForKeyboardShortcut);
