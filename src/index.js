3f276c0d9c5800001c0aa4c553793c0e2544ebda
Aurooba Ahmed <hey@aurooba.com>
2023-05-02 12:13:37 -0600

    separate renders of quickpost and viewpost

--- src/index.js
+++ src/index.js
@@ -26,49 +26,69 @@ subscribe(() => {
 	const quickpostbutton = document.querySelector(
 		"#createwithrani-quick-post-button-wrapper"
 	);
+	const viewpostbutton = document.querySelector(
+		"#createwithrani-view-post-button-wrapper"
+	);
 
 	// If the Quick Post Button already exists, skip render
 	if (quickpostbutton) {
 		return;
+	} else {
+		domReady(() => {
+			const editorToolbar = document.querySelector(
+				".edit-post-header__toolbar"
+			);
+
+			// If toolbar doesn't exist, we can't continue
+			if (!editorToolbar) {
+				return;
+			}
+
+			// So turns out you can't append to an existing container without
+			// using dangerouslySetInnerHTML, which..I don't want to use.
+			const buttonWrapper = document.createElement("div");
+			buttonWrapper.id = "createwithrani-quick-post-button-wrapper";
+			buttonWrapper.style.cssText = "display:flex;";
+
+			// add empty div to the toolbar so we can fill it.
+			editorToolbar.appendChild(buttonWrapper);
+
+			render(
+				<QuickPostButton visibility={true} />,
+				document.getElementById(
+					"createwithrani-quick-post-button-wrapper"
+				)
+			);
+		});
+	}
+	if (viewpostbutton) {
+		return;
+	} else {
+		domReady(() => {
+			const editorToolbarSettings = document.querySelector(
+				".edit-post-header__settings"
+			);
+			const viewButtonWrapper = document.createElement("div");
+			viewButtonWrapper.id = "createwithrani-view-post-button-wrapper";
+			viewButtonWrapper.style.cssText = "display:flex;";
+
+			// If toolbar doesn't exist, we can't continue
+			if (!editorToolbarSettings) {
+				return;
+			}
+
+			// add empty div to the toolbar so we can fill it.
+			insertAfter(editorToolbarSettings.children[1], viewButtonWrapper);
+
+			render(
+				<ViewPostButton />,
+				document.getElementById(
+					"createwithrani-view-post-button-wrapper"
+				)
+			);
+		});
 	}
 
-	domReady(() => {
-		const editorToolbar = document.querySelector(
-			".edit-post-header__toolbar"
-		);
-
-		const editorToolbarSettings = document.querySelector(
-			".edit-post-header__settings"
-		);
-		const viewButtonWrapper = document.createElement("div");
-		viewButtonWrapper.id = "createwithrani-view-post-button-wrapper";
-		viewButtonWrapper.style.cssText = "display:flex;";
-
-		// If toolbar doesn't exist, we can't continue
-		if (!editorToolbar) {
-			return;
-		}
-
-		// So turns out you can't append to an existing container without
-		// using dangerouslySetInnerHTML, which..I don't want to use.
-		const buttonWrapper = document.createElement("div");
-		buttonWrapper.id = "createwithrani-quick-post-button-wrapper";
-		buttonWrapper.style.cssText = "display:flex;";
-
-		// add empty divs to the toolbars so we can fill it.
-		editorToolbar.appendChild(buttonWrapper);
-		insertAfter(editorToolbarSettings.children[1], viewButtonWrapper);
-
-		render(
-			<QuickPostButton visibility={true} />,
-			document.getElementById("createwithrani-quick-post-button-wrapper")
-		);
-
-		render(
-			<ViewPostButton />,
-			document.getElementById("createwithrani-view-post-button-wrapper")
-		);
-	});
 });
 
 document.addEventListener("keydown", listenForKeyboardShortcut);
